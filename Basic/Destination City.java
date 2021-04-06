f518d95e7dd7483e5010e0702febfa31b8fa9493
Nehal Sharma <nehalsharma.cse@gmail.com>
2021-04-06 20:10:24 +0530

    Create Destination City.java

---
+++ Basic/Destination City.java
@@ -0,0 +1,21 @@
+/*You are given the array paths, where paths[i] = [cityAi, cityBi] means there exists a direct path going from cityAi to cityBi. 
+Return the destination city, that is, the city without any path outgoing to another city.
+
+It is guaranteed that the graph of paths forms a line without any loop, therefore, there will be exactly one destination city.*/
+
+
+class Solution {
+    public String destCity(List<List<String>> paths) {
+        ArrayList<String> fromCities = new ArrayList<String>();
+        String ans = "";
+        String res = "";
+        for(List l : paths)
+            fromCities.add(l.get(0).toString());
+        for(List l : paths){
+            res = l.get(1).toString();
+            if(!(fromCities.contains(res)))
+                return res;
+        }
+        return res;
+    }
+}
