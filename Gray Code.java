02359850ec3e72567cc272fe39dfde2c25409e21
Nehal Sharma <nehalsharma.cse@gmail.com>
2021-06-09 21:15:24 +0530

    Create Gray Code.java

---
+++ Gray Code.java
@@ -0,0 +1,35 @@
+//Not time efficient
+class Solution {
+    public List<Integer> grayCode(int a) {
+	    List<Integer> output = new ArrayList<Integer>();
+	   // ArrayList<String> binary = new ArrayList<String>();
+	    ArrayList<String> res = findGrayCode(a);
+	    int len = res.size();
+	    for(int i=0; i<len; i++)
+	        output.add(Integer.parseInt(res.get(i), 2));
+	    return output;
+	}
+	
+	public ArrayList<String> findGrayCode(int a){
+	    if(a==1){
+	        ArrayList<String> res = new ArrayList<String>();
+	        res.add("0");
+	        res.add("1");
+	        return res;
+	    }
+	    
+	    ArrayList<String> prevRes = findGrayCode(a-1);
+	    ArrayList<String> res = new ArrayList<String>();
+	    int len = prevRes.size();
+	    for(int i=0; i<len; i++){
+	        String str = prevRes.get(i);
+	        res.add("0"+str);
+	    }
+	    for(int i=len-1; i>=0; i--){
+	        String str = prevRes.get(i);
+	        res.add("1"+str);
+	    }
+	    
+	    return res;
+	}
+}
